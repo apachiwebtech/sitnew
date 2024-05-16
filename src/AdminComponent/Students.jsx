741826e0885f5096e9ef7a8c9b669d206522b589
akshay1625 <148771971+apachiwebtech@users.noreply.github.com>
2024-05-16 16:30:00 +0530

    student changes

--- src/AdminComponent/Students.jsx
+++ src/AdminComponent/Students.jsx
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { BASE_URL } from './BaseUrl'
 import EditIcon from "@mui/icons-material/Edit";
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+import InnerHeader from './InnerHeader';
+import axios from 'axios';
+import { Switch } from '@mui/material';
 
 const Students = () => {
 
-    const [students, setStudents] = useState([])
-    const getStudentData = async () => {
+    const [onlineAdmissions, setOnlineAdmissions] = useState([])
+    const label = { inputProps: { 'aria-label': 'Color switch demo' } };
+    const getOnlineAdmissions = async () => {
         const response = await fetch(`${BASE_URL}/getStudents`, {
             method: 'GET',
             headers: {
@@ -15,16 +20,28 @@ const Students = () => {
         })
 
         const data = await response.json();
-        setStudents(data);
+        setOnlineAdmissions(data);
         console.log(data);
     }
 
     useEffect(() => {
-        getStudentData();
+        getOnlineAdmissions();
     }, [])
     const handleUpdate = () => {
         console.log('hehehe')
     }
+
+    const handleswitchchange = (value,Inquiry_Id) =>{
+        const newval = value == 0 ? 1 : 0
+    
+        axios.post(`${BASE_URL}/data_status` , {status : newval, Inquiry_Id : Inquiry_Id, table_name : "Student_Master"})
+        .then((res)=>{
+            console.log(res)
+            getOnlineAdmissions()
+        })
+     }
+
+
     const columns = [
         {
             field: 'index',
@@ -50,41 +67,67 @@ const Students = () => {
             renderCell: (params) => {
                 return (
                     <>
-                        <EditIcon style={{ cursor: "pointer" }} onClick={() => handleUpdate(params.row.id)} />
+                        <Link to={`/onlineadmissionform/personalinfo/${params.row.Student_Id}`}><EditIcon style={{ cursor: "pointer" }} /></Link>
+                        <Switch {...label} onChange={() => handleswitchchange(params.row.isActive,params.row.id )} defaultChecked={params.row.isActive == 0 ? false : true} color="secondary" />
                     </>
                 )
             }
         },
     ];
 
-    const rowsWithIds = students.map((row, index) => ({ index: index + 1, ...row }));
-    console.log(rowsWithIds);
+    const rowsWithIds = onlineAdmissions.map((row, index) => ({ index: index + 1, ...row }));
+
+
     return (
         <div className="container-fluid page-body-wrapper col-lg-10">
+            <InnerHeader/>
             <div className="main-pannel">
-                <div className="content-wrapper">
-                    <DataGrid
-                        rows={rowsWithIds}
-                        columns={columns}
-                        disableColumnFilter
-                        disableColumnSelector
-                        disableDensitySelector
-                        rowHeight={50}
-                        getRowId={(row) => row.Present_Mobile}
-                        initialState={{
-                            pagination: {
-                                paginationModel: { pageSize: 10, page: 0 },
-                            },
-                        }}
-                        slots={{ toolbar: GridToolbar }}
-                        slotProps={{
-                            toolbar: {
-                                showQuickFilter: false,
-                            },
-                        }}
-                    />
+                <div className="content-wrapper ">
+                    <div className="row">
+
+                        <div className="col-lg-12">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className='d-flex justify-content-between gap-3' style={{ width: "100%", padding: "10px 0" }}>
+                                        <div >
+                                            <h4 class="card-title">Student Information</h4>
+                                        </div>
+                            
+
+                                    </div>
+
+                                    <div>
+                                        <DataGrid
+                                            rows={rowsWithIds}
+                                            columns={columns}
+                                            disableColumnFilter
+                                            disableColumnSelector
+                                            disableDensitySelector
+                                            rowHeight={37}
+                                            getRowId={(row) => row.Present_Mobile}
+                                            initialState={{
+                                                pagination: {
+                                                    paginationModel: { pageSize: 10, page: 0 },
+                                                },
+                                            }}
+                                            slots={{ toolbar: GridToolbar }}
+                                            slotProps={{
+                                                toolbar: {
+                                                    showQuickFilter: true,
+                                                },
+                                            }}
+                                        />
+                                    </div>
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+          
         </div>
     )
 }
