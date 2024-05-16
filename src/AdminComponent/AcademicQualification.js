11539e9b16673853af4e5ce4c1d359363e4f5d22
apachiweb <abhishek@apachiweb.co.in>
2024-05-16 08:34:32 +0530

    Changes by Abhishek

--- src/AdminComponent/AcademicQualification.js
+++ src/AdminComponent/AcademicQualification.js
@@ -1,9 +1,46 @@
 import React from 'react'
 import OnlineAdmissionForm from './OnlineAdmissionForm'
+import { Link, useParams } from 'react-router-dom';
+import { BASE_URL } from './BaseUrl';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import InnerHeader from './InnerHeader';
+import decryptedUserId from '../Utils/UserID';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Button, Card, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+
 
 const AcademicQualification = () => {
   return (
-    <div><OnlineAdmissionForm/>AcademicQualification</div>
+    <div className="container-fluid page-body-wrapper">
+        <InnerHeader />
+            
+        
+            <div className="main-panel">
+
+                <div className="content-wrapper">
+
+                <OnlineAdmissionForm/>
+                 
+                    <div className="row">
+                        <div className="col-lg-12 grid-margin">
+                            <div className="card">
+
+                                <div className='container-fluid'>
+                                    Table Grid
+                                </div>
+                            </div>
+                        </div>
+                     
+                    </div>
+                </div>
+            </div >
+        </div >
   )
 }
 
