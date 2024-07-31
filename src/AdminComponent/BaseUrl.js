15d62ac94845b151a9d9e24cb9ab16baec6c44d4
apache <148771971+apachiwebtech@users.noreply.github.com>
2024-07-31 11:45:14 +0530

    base

--- src/AdminComponent/BaseUrl.js
+++ src/AdminComponent/BaseUrl.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "https://nodeapp.sitsuvidya.in/nodeapp"
 // export const BASE_URL = "https://sitnode.thetalentclub.co.in"
 export const IMG_URL = "https://thetalentclub.co.in/ecomuploads"
-export const BASE_URL = "http://localhost:8081"
+export const BASE_URL = "http://localhost:8081/nodeapp"
