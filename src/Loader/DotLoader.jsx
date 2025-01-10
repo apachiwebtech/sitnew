c39be4e59f4d153c7cfbed4b569e8f713a07a0cc
satyamsatkar <apachiwebtechnologyawt@gmail.com>
2025-01-10 17:09:36 +0530

    addlatestcvupdated

---
+++ src/Loader/DotLoader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DotLoader = () => {
+  return (
+    <div className='' style={{ height: "100vh", background: "#fff", position: "relative" }}>
+      <div className='dot-loader' style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)" }}>
+
+      </div>
+    </div>
+  )
+}
+
+export default DotLoader
+
